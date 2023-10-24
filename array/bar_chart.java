732e03869a1d47be6927c14814e8439391000bac
JAHANWEE <jahanweeraj20@gmail.com>
2023-10-25 03:51:04 +0530

    incomplete

---
+++ array/bar_chart.java
@@ -0,0 +1,15 @@
+package array;
+
+import java.util.Scanner;
+
+public class bar_chart {
+    public static void main(String []args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int arr[]= new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]= scn.nextInt();
+        }
+        
+    }
+}
