ccfe3cdbf4bfd0a37bf512bfa3abf288775495a1
JAHANWEE <jahanweeraj20@gmail.com>
2023-10-29 19:06:05 +0530

    strings input

---
+++ stringss/input.java
@@ -0,0 +1,9 @@
+package stringss;
+import java.util.*;
+public class input {
+    public static void main(String[] args) {
+         Scanner scn = new Scanner(System.in);
+         String str ="Hello i am a string";
+         System.out.println(str);
+    }
+}
