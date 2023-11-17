8a48407aa70fb5d3a1fbb4bf6dfde41fcad8ef40
JAHANWEE <jahanweeraj20@gmail.com>
2023-11-18 01:23:33 +0530

    operators

--- basic/constant.java
+++ basic/constant.java
@@ -7,5 +7,9 @@ public class constant {
         Scanner scn = new Scanner(System.in);
         final int sb = 56;  //final makes he variable  as a constant
         System.out.println(sb);
+
+        int a=10;
+        int b =5;
+        System.out.println(a/(float)b);   //2.0
     }
 }
