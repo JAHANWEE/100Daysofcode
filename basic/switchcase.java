afbd5df09f5155e549e053e874e4598502f10552
JAHANWEE <jahanweeraj20@gmail.com>
2023-11-18 01:40:14 +0530

    switch case

---
+++ basic/switchcase.java
@@ -0,0 +1,50 @@
+package basic;
+
+import java.util.Scanner;
+
+public class switchcase {
+    public static void main(String [] args){
+        Scanner scn = new Scanner(System.in);
+        int month = scn.nextInt();
+
+         switch(month){
+            case 1->{
+                System.out.println("January");
+            }
+             case 2->{
+                System.out.println("February");
+            }
+             case 3->{
+                System.out.println("March");
+            }
+             case 4->{
+                System.out.println("April");
+            }
+             case 5->{
+                System.out.println("May");
+            }
+             case 6->{
+                System.out.println("June");
+            }
+             case 7->{
+                System.out.println("July");
+            }
+             case 8->{
+                System.out.println("August");
+            }
+             case 9->{
+                System.out.println("September");
+            }
+             case 10->{
+                System.out.println("October");
+            }
+             case 11->{
+                System.out.println("November");
+            }
+             case 12->{
+                System.out.println("December");
+            }
+
+         }
+    }
+}
