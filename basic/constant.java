468778c66dc9c98e708259ae31b075c45fab53fa
JAHANWEE <jahanweeraj20@gmail.com>
2023-11-18 01:05:33 +0530

    final keyword

---
+++ basic/constant.java
@@ -0,0 +1,11 @@
+package basic;
+
+import java.util.Scanner;
+
+public class constant {
+    public static void main(String [] args){
+        Scanner scn = new Scanner(System.in);
+        final int sb = 56;  //final makes he variable  as a constant
+        System.out.println(sb);
+    }
+}
