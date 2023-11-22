46ad25d76f8345d5d41644b3a52012faba009665
JAHANWEE <jahanweeraj20@gmail.com>
2023-11-23 04:15:07 +0530

    string

--- stringss/stringbuild.java
+++ stringss/stringbuild.java
@@ -5,7 +5,17 @@ public class stringbuild {
         sb.append(" and hey");
 
         System.out.println(sb);
+        sb.insert(5 , " mello");
+        System.out.println(sb);
 
-        
+        sb.replace(6 , 11 , "trello");
+        System.out.println(sb);
+
+        sb.delete( 6 , 12);
+        System.out.println(sb);
+
+        String str = sb.toString();
+        System.out.println(str);
+
     }
 }
