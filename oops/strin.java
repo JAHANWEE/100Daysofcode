3d8452f68f2f691cd7858730c97b04778b29e432
JAHANWEE <jahanweeraj20@gmail.com>
2023-12-03 03:28:43 +0530

    string

--- oops/strin.java
+++ oops/strin.java
@@ -2,7 +2,19 @@ package oops;
 
 public class strin {
     public static void main(String[] args){
-        String name="navin";
-        for(int i=0)
+        StringBuffer sb = new StringBuffer("hello");
+        System.out.println(sb.capacity());   // gives 16 as a buffer space if no string is added. and if string hello is aded gives 21.
+        System.out.println(sb.length());
+        System.out.println(sb.append(" world"));  //hello world
+        String str = sb.toString(); // to convert string buffer to string 
+        System.out.println(str);
+
+        sb.deleteCharAt(2);
+        System.out.println(sb);    // gives helo world
+
+        sb.insert(2, "new");
+        System.out.println(sb);   //henewlo world
     }
 }
+
+//stringbuffer is thread safe and stringbuider is not
