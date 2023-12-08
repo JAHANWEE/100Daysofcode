59d38a10e0b1e7da48d5869d633513345320e920
JAHANWEE <jahanweeraj20@gmail.com>
2023-12-09 00:40:53 +0530

    2d

---
+++ twod/twod.java
@@ -0,0 +1,23 @@
+package twod;
+
+public class twod {
+    public static void main(String []args){
+        int[][] a={
+            {5 , 6, 4},
+            {5 , 2, 3},
+            {4 , 9},
+        };
+        System.out.println(a[0][0]);
+        System.out.println(a[0][1]);
+        System.out.println(a[0][2]);
+
+        System.out.println(a[1][0]);  // row , column
+        System.out.println(a[1][1]);
+        System.out.println(a[1][2]);
+
+        System.out.println(a[2][0]);
+        System.out.println(a[2][1]);
+
+
+    }
+}
---
+++ twod/twodd.java
@@ -0,0 +1,7 @@
+package twod;
+
+public class twodd {
+    public static void main(String []args){
+        int [][] a = new int[3][4];
+    }
+}
