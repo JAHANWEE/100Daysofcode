0945f8cd55dccdaa04843f9bb7f544503817e6d3
JAHANWEE <jahanweeraj20@gmail.com>
2023-12-10 19:35:11 +0530

    bigInteger

--- objects/bigint.java
+++ objects/bigint.java
@@ -1,7 +1,20 @@
 package objects;
 
+import java.math.BigInteger;
+
 public class bigint {
     public static void main(String[] args){
-        
+        BigInteger a = new BigInteger("500000000000000000");  //as long cannot store it
+        BigInteger b = new BigInteger("1000000000000000000");
+
+        System.out.println(a);
+        System.out.println(b);
+        BigInteger c = a.add(b);
+        System.out.println(c);
+
+        System.out.println(a.subtract(b));
+        System.out.println(a.multiply(b));
+
+         
     }
 }
