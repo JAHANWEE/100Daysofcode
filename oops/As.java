3bf9beee4d4513b29aa7f43446cf7dcbe01b8519
JAHANWEE <jahanweeraj20@gmail.com>
2023-12-18 23:32:26 +0530

    access modifies

---
+++ oops/As.java
@@ -0,0 +1,13 @@
+package oops;
+
+
+public  class As {
+// private can be used only in same class
+
+// default can be acccessd in same class+same package subclass +same package non sub class but not in different package
+
+//protected can be  used in : same class+subclasses+same package+subclass but not in a different package non-subclass
+
+//public can be used everywhere
+int marks=7;
+}
---
+++ oops/acess.java
@@ -0,0 +1,8 @@
+package oops;
+
+public class acess {
+    public static void main(String[] args){
+        As obj = new As();
+       System.out.println( obj.marks);
+    }
+}
