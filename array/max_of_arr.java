c1ed1c2ff4ef6e7f4e7d028f676e9e72c6ed1c29
JAHANWEE <jahanweeraj20@gmail.com>
2023-12-21 03:40:46 +0530

    max of array

--- array/max_of_arr.java
+++ array/max_of_arr.java
@@ -2,12 +2,12 @@ package array;
 
 public class max_of_arr {
     public static void main(String []args){
-        int[] arr= {10 , -1, 3 , -7};
+        int[] arr= {-10 , -1, 0 , -7};
         int r =max(arr);
         System.out.println(r);
     }
     public static  int max(int[] arr){
-        int max=0;
+        int max=Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
           if(arr[i]>max){
             max=arr[i];
@@ -16,3 +16,4 @@ public class max_of_arr {
         return max;
     }
 }
+//hint :minus agar saare inputs ho toh cant set max to zero so set to Integr.MIN_VALUE
